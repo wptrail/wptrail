d56f68c7cecc828cbc0343ca4e920f75b14ea53e
Nasyrov Evgenii <inasyrov@ya.ru>
2021-08-11 12:16:44 +0100

    Use mockery's test case to extend from

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,16 +2,13 @@
 
 namespace WPTrail\Tests;
 
-use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
-use PHPUnit\Framework\TestCase as BaseTestCase;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
 
 use function Brain\Monkey\setUp;
 use function Brain\Monkey\tearDown;
 
-abstract class TestCase extends BaseTestCase
+abstract class TestCase extends MockeryTestCase
 {
-    use MockeryPHPUnitIntegration;
-
     protected function setUp(): void
     {
         setUp();
