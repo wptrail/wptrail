9cc02b4fad7d226786c9932d6b9465135554ba32
Nasyrov Evgenii <inasyrov@ya.ru>
2021-08-11 14:22:54 +0100

    Add example test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace WPTrail\Tests;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
